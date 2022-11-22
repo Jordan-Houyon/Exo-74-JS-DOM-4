6be61fa07540954da75bc6828d585f7a936b8e5f
jordan houyon <116800565+Jordan-Houyon@users.noreply.github.com>
2022-11-22 14:00:25 +0100

    EXO 74 FINI

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,10 +1,16 @@
 //Votre code ci dessous
+let monimage=document.getElementById("cat-pic");
+let chat= document.getElementById("cat-chat");
 
+function miauler(){
+    chat.innerText= "Bruits de ron-rons";
+}
 
-
-
-
-
+monimage.addEventListener("click",function (){
+    miauler()
+    //console.log(' le bouton a été déclenché')
+    }
+)
 
 
 
--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,10 +1,9 @@
 let libButton = document.getElementById('lib-button');
 let libIt = function() {
     let storyDiv = document.getElementById("story");
-    storyDiv.innerHTML = "??Your story here";
+    storyDiv.innerHTML = "un(e) " + document.getElementById("noun").value + " " + document.getElementById("adjective").value + " a mangé(e) " + document.getElementById("person").value;
 };
 libButton.addEventListener('click', libIt);
-
 
 
 
